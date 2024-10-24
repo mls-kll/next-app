731852f8d393c9c931c2f79ced9a9b3750ed09a0
Milos Kallai <milos.kallai@one-beyond.com>
2024-10-24 11:23:36 +0200

    feat: add nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,7 @@
-import Link from "next/link";
+
 
 export default function Home() {
   return (
-    <>
-      <h1>MAIN</h1>
-      <div>
-        <ul>
-          <li>
-            <Link href="/projects/project-1">Project 1</Link>
-          </li>
-          <li>
-            <Link href="/projects/project-2">Project 2</Link>
-          </li>
-          <li>
-            <Link href="/projects/project-3">Project 3</Link>
-          </li>
-        </ul>
-      </div>
-    </>
+   <h1>home page</h1>
   );
 }
