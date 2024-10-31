26aefba4092c74905b59c6a6086d979a69d19d62
Milos Kallai <milos.kallai@one-beyond.com>
2024-10-31 16:14:54 +0100

    feat: add contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,11 @@
+import { ContactForm } from "@/components/contact-form/contact-form";
+
 export default function ContactPage() {
   // form spree
-  return <h1>Contact</h1>;
+  return (
+    <>
+      <h1>Contact</h1>
+      <ContactForm />
+    </>
+  );
 }
---
+++ components/contact-form/contact-form.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useForm, ValidationError } from "@formspree/react";
+import styles from "./contact-form.module.scss";
+import { Button } from "react-bootstrap";
+
+export const ContactForm = () => {
+  const [state, handleSubmit] = useForm("xdkoeepp");
+  if (state.succeeded) {
+    return <div>Thank you for your message</div>;
+  }
+  return (
+    <form onSubmit={handleSubmit} className={styles.contactFormRoot}>
+      <label htmlFor="full-name">Full Name</label>
+      <input
+        type="text"
+        name="name"
+        id="full-name"
+        placeholder="First and Last"
+        required
+        control-id="ControlID-2"
+      />
+      <ValidationError field="full-name" prefix="Name" errors={state.errors} />
+
+      <label htmlFor="email-address">Email Address</label>
+      <input
+        type="email"
+        name="_replyto"
+        id="email-address"
+        placeholder="email@domain.tld"
+        required
+        control-id="ControlID-3"
+      />
+      <ValidationError
+        field="email-address"
+        prefix="Email"
+        errors={state.errors}
+      />
+      <label htmlFor="message">Message</label>
+      <textarea
+        rows={5}
+        name="message"
+        id="message"
+        placeholder="Your message"
+        required
+        control-id="ControlID-4"
+      />
+      <ValidationError field="message" prefix="Message" errors={state.errors} />
+
+      <Button
+        className={styles.submitButton}
+        variant="primary"
+        size="sm"
+        type="submit"
+        disabled={state.submitting}
+      >
+        Close
+      </Button>
+    </form>
+  );
+};
